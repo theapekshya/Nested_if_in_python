ed48240d541b39948c3f404a8e82938db2560289
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-15 21:52:31 +0545

    Nested_if_in_python

---
+++ nested_if.py
@@ -0,0 +1,17 @@
+# nested if-elif condition example
+p=67
+if p>=90:
+    print('distiction')
+elif p>=80:
+        print('first div')
+elif p>=70:
+        print('second div')
+elif p>=60:
+        print('pass')
+else:
+     print('fail')
+
+    
+
+
+
